896bc39d97e19a62c1b707e4cb5920af0c98e5f8
brunomotadev <brunomotadev@gmail.com>
2021-07-27 05:38:23 -0300

    criado a aplicacao dotnetcore e exercicio

--- main.cs
+++ main.cs
@@ -1,44 +1,45 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+// APENAS PRA ANOTACAO
+//using System;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Text;
+// using System.Threading.Tasks;
 
-namespace Programming
-{
-    class Program {
-        static void Main(string[] args) {
-            //Tipos de dados
-            int meuInteiro = 20;
-            char meuChar = 'a';
-            string minhaString = "palavra";
+// namespace Programming
+// {
+//     class Program {
+//         static void Main(string[] args) {
+//             //Tipos de dados
+//             int meuInteiro = 20;
+//             char meuChar = 'a';
+//             string minhaString = "palavra";
 
-            float meuFloat = 10.5f; //f para float
-            double meuDouble = 20.5;
-            decimal meuDecimal = 20.5m; //m para decimal
+//             float meuFloat = 10.5f; //f para float
+//             double meuDouble = 20.5;
+//             decimal meuDecimal = 20.5m; //m para decimal
 
-            bool meuBool = true; //valor booleano true ou false
+//             bool meuBool = true; //valor booleano true ou false
 
-            int i = 3;
-            Console.WriteLine(i);
-            Console.WriteLine(i++);
-            Console.WriteLine(i);
+//             int i = 3;
+//             Console.WriteLine(i);
+//             Console.WriteLine(i++);
+//             Console.WriteLine(i);
 
-            int a = 1 + 2 + 3;
-            int b =6;
-            Console.WriteLine(a == b);
+//             int a = 1 + 2 + 3;
+//             int b =6;
+//             Console.WriteLine(a == b);
 
-            char c1 = 'a';
-            char c2 = 'A';
-            Console.WriteLine(c1 == c2);
-            Console.WriteLine(c1 == char.ToLower(c2));
+//             char c1 = 'a';
+//             char c2 = 'A';
+//             Console.WriteLine(c1 == c2);
+//             Console.WriteLine(c1 == char.ToLower(c2));
 
-            string S1 = "hello!";
-            string s2 = "HeLLo!";
+//             string S1 = "hello!";
+//             string s2 = "HeLLo!";
 
-            string s3 = "Hello!";
-            Console.WriteLine(S1 == s3);
+//             string s3 = "Hello!";
+//             Console.WriteLine(S1 == s3);
 
-        }
-    }
-}
+//         }
+//     }
+// }
