d71dcc1fbfdfdaa7c3d8b534de31020823475c74
brunomotadev <brunomotadev@gmail.com>
2021-07-27 06:00:56 -0300

    adicionado descrição do exercicio em comentario

--- Exemplo1.cs
+++ Exemplo1.cs
@@ -1,3 +1,10 @@
+// Adicione uma de repetição que tenha 5 execuções, utilizando a estrutura de controle de sua preferência.
+// Adicione uma nova variável para o controle das repetições.
+// A cada impressão, informe o número da execução em curso.
+// Ao final de cada volta, adicione 10 unidades a cada uma, das três variáveis definidas anteriormente.
+
+
+
 using System;
 
 public class Example {
